6bdf57a1fcc014b2d2b6ae6e45c1b6464c2bb221
도준영 <127960979+kaco2301@users.noreply.github.com>
2024-03-12 18:56:10 +0900

    0312

---
+++ Assets/Editor/MyCustomEditor.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class MyCustomEditor : EditorWindow
+{
+    [MenuItem("Window/UI Toolkit/MyCustomEditor")]
+    public static void ShowExample()
+    {
+        MyCustomEditor wnd = GetWindow<MyCustomEditor>();
+        wnd.titleContent = new GUIContent("MyCustomEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/MyCustomEditor.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/MyCustomEditor.uss");
+        VisualElement labelWithStyle = new Label("Hello World! With Style");
+        labelWithStyle.styleSheets.Add(styleSheet);
+        root.Add(labelWithStyle);
+    }
+}
