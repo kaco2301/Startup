7272695066e436f942f99e28e19b87c1e2d55c59
도준영 <kaco2301@gmail.com>
2024-03-17 18:11:25 +0900

    0317

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    public string readFileName = "";
+    public string readHeaderName = "";
+
+    private void Start()
+    {
+        List<Dictionary<string, object>> data_Dialog = CSVReader.Read(readFileName);
+
+        for (int i = 0; i < data_Dialog.Count; i++) 
+        {
+            Debug.Log(data_Dialog[i][readHeaderName].ToString()); 
+        }
+    }
+}
+
